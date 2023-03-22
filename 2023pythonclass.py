08601729e52a8dd51d5afe9ac3ca142d94b50c8e
binary110100001 <dmlcks0417@naver.com>
2023-03-22 11:15:25 +0900

    intial class

--- 2023pythonclass.py
+++ 2023pythonclass.py
@@ -1,1 +1,4 @@
-print("hello world")
+import random
+com= random.randrange(3)
+user= int(input('가위0 바위1 보2 선택: '))
+print('user= %d, com= %d' % (user, com))
---
+++ p3.py
@@ -0,0 +1,8 @@
+import random
+com= random.randrange(4)
+if com==0:
+    print('제주도')
+elif com==1:
+    print('사이판')
+else:
+    print('하와이')
